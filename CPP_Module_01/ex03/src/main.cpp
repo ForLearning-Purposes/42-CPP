1a0d8d1c6f572977bca67b1251ed8afe79e93cd8
Monika <128793184+LaDeMonika@users.noreply.github.com>
2025-03-01 18:32:13 +0000

    ADD: just a comment

--- CPP_Module_01/ex03/src/main.cpp
+++ CPP_Module_01/ex03/src/main.cpp
@@ -2,6 +2,11 @@
 #include "../include/HumanB.hpp"
 #include "../include/Weapon.hpp"
 
+/*
+In which case do you think it would be best to use a pointer to
+Weapon? And a reference to Weapon? Why? Think about it before
+starting this exercise.
+*/
 int main()
 {
     {
