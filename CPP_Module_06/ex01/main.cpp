0880ca2d26c73b32de30e435c9e5e17bc841fb2b
Monika Simic <msimic@c2r1p10.42vienna.com>
2025-05-27 15:17:58 +0200

    added test to serialized data

--- CPP_Module_06/ex01/main.cpp
+++ CPP_Module_06/ex01/main.cpp
@@ -4,4 +4,9 @@ int main() {
     Data employee_data = {123342, "Monika Simic"};
 
     std::cout << "Original Data: " << employee_data.employee_ID << ", " << employee_data.employee_name << std::endl;
+
+    uintptr_t ser_data = Serializer::serialize(&employee_data);
+    std::cout << "Serialized Data: " << ser_data << std::endl; //
+    Data* des_data = Serializer::deserialize(ser_data);
+    std::cout << "Deserialized Data: " << des_data->employee_ID << ", " << des_data->employee_name << std::endl;
 }
